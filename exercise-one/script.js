bf9dade37aebb2bd75087e16664a968df980a119
oziomarc <oziomarc@gmail.com>
2022-09-15 19:20:14 -0400

    final exercise

---
+++ exercise-one/script.js
@@ -0,0 +1,19 @@
+// let > var
+
+const coolArrowFunction = () => false; // this is a variable
+// same as function coolFunction () { return false}
+
+const coolObj = {
+    hi: "hello",
+    coolNumber: 21
+}
+
+const coolJson = [
+{
+    hi: 'hi',
+},
+{
+    hi: 'hello'
+}
+]
+
