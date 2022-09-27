0566c9e08462808d830a5da97d2abaeb9d7f6d90
Ozioma Chukwukeme <oziomarc@gmail.com>
2022-09-27 09:14:50 -0400

    fixed image and added url parameters

--- exercise-2-part2/src/components/ArticleCard.js
+++ exercise-2-part2/src/components/ArticleCard.js
@@ -9,8 +9,16 @@ function ArticleCard ({
 }) {
     return (
         <div className="articleCard">
-            <div className="">
-                
+            <div className="articleCard--image">
+                <img src={imageSrc} alt={imageAlt}/>
+            </div>
+            <div className="articleCard--text">
+                <h2>{title}</h2>
+                <p className="date">{date}</p>
+                <p className="blurb">{blurb}</p>
+                <p className="link">
+                    <a href={`/article/${id}`}>Read More</a>
+                </p>
             </div>
         </div>
     )
